7edf4bb20b3c50cbb8b0b247f170eb2ac69f580d
Horváth Balázs <balazs630@icloud.com>
2016-08-02 23:34:04 +0200

    Fixed static variable names according to naming conventions

--- balazs.horvath/1-homework-webshop/src/test/java/hu/oktatas/java/ee/webshop/beans/UserDTOTest.java
+++ balazs.horvath/1-homework-webshop/src/test/java/hu/oktatas/java/ee/webshop/beans/UserDTOTest.java
@@ -10,7 +10,6 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.Assert;
-import org.junit.Before;
 
 public class UserDTOTest {
 
@@ -18,15 +17,15 @@ public class UserDTOTest {
     private static Validator validator;
 
     private final static Calendar REGTIME = Calendar.getInstance();
-    private final static Calendar DATEOFBIRTHVALID = Calendar.getInstance();
-    private final static Calendar DATEOFBIRTHINVALID = Calendar.getInstance();
+    private final static Calendar DATE_OF_BIRTH_VALID = Calendar.getInstance();
+    private final static Calendar DATE_OF_BIRTH_INVALID = Calendar.getInstance();
 
     @BeforeClass
     public static void setUpClass() {
         vf = Validation.buildDefaultValidatorFactory();
         validator = vf.getValidator();
-        DATEOFBIRTHVALID.add(Calendar.YEAR, -10); //present time -10 years
-        DATEOFBIRTHINVALID.add(Calendar.DATE, +1); //present time +1 day
+        DATE_OF_BIRTH_VALID.add(Calendar.YEAR, -10); //present time -10 years
+        DATE_OF_BIRTH_INVALID.add(Calendar.DATE, +1); //present time +1 day
         REGTIME.add(Calendar.SECOND, -1); //present time -1 sec (to ensure it's in the past)
     }
 
@@ -129,7 +128,7 @@ public class UserDTOTest {
     @Test
     public void ValidDateOfBirth() {
         UserDTO user = new UserDTO("testuser", "Passs1234", "user@domain.com", REGTIME);
-        user.setDateOfBirth(DATEOFBIRTHVALID);
+        user.setDateOfBirth(DATE_OF_BIRTH_VALID);
         Set<ConstraintViolation<UserDTO>> violations = validator.validate(user);
         Assert.assertEquals(0, violations.size());
     }
@@ -137,7 +136,7 @@ public class UserDTOTest {
     @Test
     public void InvalidDateOfBirth() {
         UserDTO user = new UserDTO("testuser", "Passs1234", "user@domain.com", REGTIME);
-        user.setDateOfBirth(DATEOFBIRTHINVALID);
+        user.setDateOfBirth(DATE_OF_BIRTH_INVALID);
         Set<ConstraintViolation<UserDTO>> violations = validator.validate(user);
         Assert.assertEquals(1, violations.size());
         Assert.assertEquals(user, violations.iterator().next().getInvalidValue());
