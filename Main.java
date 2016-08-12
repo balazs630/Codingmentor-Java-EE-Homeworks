6e4f8fee587171c29f69d270da284fc18d016ba3
Horváth Balázs <balazs630@icloud.com>
2016-08-12 17:14:22 +0200

    Update README.md

---
+++ Main.java
@@ -0,0 +1,9 @@
+package xyz.codingmentor.helloworld;
+
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, Codingmentor Java EE Training");
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# homeworks
-Homeworks of codingmentor training
+# Homeworks of Codingmentor Java EE training
+
+Weekly homeworks on different branches:
+
+- EE-0
+- EE-1
+- EE-2/1
+- EE-2/2
+- jpa-hw-1
+- jpa-hw-2
